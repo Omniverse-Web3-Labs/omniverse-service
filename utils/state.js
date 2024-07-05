daf10c60a3148d3b9e207e129c28eccec3756f38
kay404 <kay20475@hotmail.com>
2024-07-05 16:34:29 +0800

    * fix empty state error

--- utils/state.js
+++ utils/state.js
@@ -25,8 +25,7 @@ class stateDB {
   }
 
   getTable(table) {
-    // return this.state[table]? this.state[table]: {};
-    return this.state[table];
+    return this.state[table]? this.state[table]: {};
   }
 
   has(table, key) {
