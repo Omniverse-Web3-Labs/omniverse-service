3e25638a267db51a19fc9cd7eb34622297d22ed0
virgil2019 <danten0402@gmail.com>
2024-07-14 16:44:56 +0800

    Add faucet of local token

--- faucet/pollService.js
+++ faucet/pollService.js
@@ -1,78 +1,162 @@
 const utils = require('../utils/utils');
 const { eip712Hash } = require('./eip712');
 const config = require('config');
+const fs = require('fs');
+const {Web3} = require('web3');
 const PENDING_TABLE_NAME = 'Pending';
 const CLAIMED_TABLE_NAME = 'Claimed';
+const LOCAL_PENDING_TABLE_NAME = 'LocalPending';
+const LOCAL_CLAIMED_TABLE_NAME = 'LocalClaimed';
 
-const polling = async (request, sender, secret) => {
+async function sendOmniverseAsset(request, sender, secret) {
   const assetId = config.get('assetId')
   const faucetAmount = config.get('faucetAmount')
-  while (1) {
-    let table = StateDB.getTable(PENDING_TABLE_NAME);
-    if (Object.keys(table).length !== 0) {
-      let networkParameters = await request.rpc('getNetworkParameters', []);
-      let eip712Domain = {
-        chainId: networkParameters.eip712.chainId,
-        name: 'Omniverse Transaction',
-        verifyingContract: networkParameters.eip712.verifyingContract,
-        version: networkParameters.eip712.version,
-      };
-      let outputs = [];
-      const addresses = Object.keys(table);
-      for (let address of addresses) {
-        outputs.push({
-          address,
-          amount: faucetAmount,
+  let table = StateDB.getTable(PENDING_TABLE_NAME);
+  if (Object.keys(table).length !== 0) {
+    let networkParameters = await request.rpc('getNetworkParameters', []);
+    let eip712Domain = {
+      chainId: networkParameters.eip712.chainId,
+      name: 'Omniverse Transaction',
+      verifyingContract: networkParameters.eip712.verifyingContract,
+      version: networkParameters.eip712.version,
+    };
+    let outputs = [];
+    const addresses = Object.keys(table);
+    for (let address of addresses) {
+      outputs.push({
+        address,
+        amount: faucetAmount,
+      });
+    }
+    if (addresses) {
+      try {
+        let preTransferData = await request.rpc('preTransfer', [
+          {
+            assetId,
+            address: sender,
+            outputs,
+          },
+        ]);
+        console.log(preTransferData)
+        let messageHash = eip712Hash(eip712Domain, {
+          asset_id: assetId,
+          ...utils.snakeCaseKeys(preTransferData),
         });
-      }
-      if (addresses) {
+
+        let signature = utils.sign(messageHash, secret);
+        let txid = '';
         try {
-          let preTransferData = await request.rpc('preTransfer', [
+          txid = await request.rpc('sendTransaction', [
             {
+              type: 'Transfer',
+              signature,
               assetId,
-              address: sender,
-              outputs,
+              ...preTransferData,
             },
           ]);
-          console.log(preTransferData)
-          let messageHash = eip712Hash(eip712Domain, {
-            asset_id: assetId,
-            ...utils.snakeCaseKeys(preTransferData),
-          });
-
-          let signature = utils.sign(messageHash, secret);
-          let txid = '';
-          try {
-            txid = await request.rpc('sendTransaction', [
-              {
-                type: 'Transfer',
-                signature,
-                assetId,
-                ...preTransferData,
-              },
-            ]);
-            for (let address of addresses) {
-              let value = StateDB.getValue(PENDING_TABLE_NAME, address);
-              StateDB.setValue(CLAIMED_TABLE_NAME, address, value);
-              StateDB.deleteValue(PENDING_TABLE_NAME, address);
-            }
-          } catch (e) {
-            console.error('sendTransaction failed:', e);
-            continue;
-          }
-          console.log('txid: ', txid);
-          while (1) {
-            await utils.sleep(2);
-            if (await request.rpc('getTransactionDetail', [txid])) {
-              console.log('Transfer to', addresses.toString(), 'complete.');
-              break;
-            }
+          for (let address of addresses) {
+            let value = StateDB.getValue(PENDING_TABLE_NAME, address);
+            StateDB.setValue(CLAIMED_TABLE_NAME, address, value);
+            StateDB.deleteValue(PENDING_TABLE_NAME, address);
           }
         } catch (e) {
-          console.error('rpc request failed:', e);
+          console.error('sendTransaction failed:', e);
+          return;
         }
+        console.log('txid: ', txid);
+        while (1) {
+          await utils.sleep(2);
+          if (await request.rpc('getTransactionDetail', [txid])) {
+            console.log('Transfer to', addresses.toString(), 'complete.');
+            break;
+          }
+        }
+      } catch (e) {
+        console.error('rpc request failed:', e);
       }
     }
+  }
+}
+
+async function sendLocalToken() {
+  const faucetAmount = config.get('localToken.faucetAmount');
+  const rpc = config.get('localToken.rpc');
+  const sk = fs.readFileSync(config.get('localToken.secret')).toString();
+  const chainId = config.get('localToken.chainId');
+  let table = StateDB.getTable(LOCAL_PENDING_TABLE_NAME);
+  console.log('table',table,Object.keys(table));
+  for (let address of Object.keys(table)) {
+    const web3 = new Web3(rpc);
+    // try {
+    const account =
+    web3.eth.accounts.privateKeyToAccount(sk).address;
+    const to = address;
+    const nonce = web3.utils.numberToHex(
+        await web3.eth.getTransactionCount(account)
+    );
+    const estimateGas = await web3.eth.estimateGas({
+        from: account,
+        to
+    });
+    // const gasPrice = await web3.eth.getGasPrice();
+    // console.log('gas: '+gas);
+    // console.log('gasPrice: '+gasPrice);
+    // console.log('estimateGas: ' + estimateGas);
+
+    let { gasPrice, maxFeePerGas, maxPriorityFeePerGas } =
+        await web3.eth.calculateFeeData();
+    const tx = {
+        account,
+        to,
+        chainId,
+        nonce,
+        value: faucetAmount,
+        gasLimit: estimateGas,
+        maxFeePerGas,
+        maxPriorityFeePerGas
+    };
+    // console.log(tx);
+
+    let signTx = await web3.eth.accounts.signTransaction(
+        tx,
+        sk
+    );
+    let ret = await web3.eth.sendSignedTransaction(signTx.rawTransaction);
+    console.log(`gasUsed: ${ret.gasUsed}`);
+    
+    // confirm
+    while (1) {
+      await utils.sleep(2);
+      const receipt = await waitForTransactionReceipt(ret.transactionHash, web3);
+      if (receipt) {
+        let value = StateDB.getValue(LOCAL_PENDING_TABLE_NAME, address);
+        StateDB.setValue(LOCAL_CLAIMED_TABLE_NAME, address, value);
+        StateDB.deleteValue(LOCAL_PENDING_TABLE_NAME, address);
+        break;
+      }
+    }
+  }
+}
+
+async function waitForTransactionReceipt(txHash, web3) {
+  let receipt;
+  while (!receipt) {
+    try {
+      receipt = await web3.eth.getTransactionReceipt(txHash);
+    } catch (e) {
+      console.log("error", e);
+    }
+
+    // Wait before checking again
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+  return receipt;
+}
+
+const polling = async (request, sender, secret) => {
+  while (1) {
+    await sendOmniverseAsset(request, sender, secret);
+    await sendLocalToken();
     await utils.sleep(5);
   }
 };
--- faucet/router.js
+++ faucet/router.js
@@ -1,6 +1,8 @@
 const ONE_DAY = 24 * 60 * 60 * 1000;
 const PENDING_TABLE_NAME = 'Pending';
 const CLAIMED_TABLE_NAME = 'Claimed';
+const LOCAL_PENDING_TABLE_NAME = 'LocalPending';
+const LOCAL_CLAIMED_TABLE_NAME = 'LocalClaimed';
 
 module.exports = async function (app) {
   app.get('/get_token', async function (req, res) {
@@ -53,6 +55,59 @@ module.exports = async function (app) {
     );
     res.send({ code: 0, message: 'Receive a faucet request successfully' });
     StateDB.setValue(PENDING_TABLE_NAME, address, currentTime);
+    return;
+  });
+
+  app.get('/get_local_token', async function (req, res) {
+    var address = req.query.address;
+    if (address.substring(0, 2) !== '0x') {
+      address = '0x' + address;
+    }
+    if (address.length !== 42) {
+      res.send({
+        code: -1,
+        message: 'Wrong address',
+      });
+      return;
+    }
+    let currentTime = new Date().getTime();
+    let lastClaimTime = currentTime;
+    if (StateDB.has(LOCAL_PENDING_TABLE_NAME, address)) {
+      lastClaimTime = StateDB.getValue(LOCAL_PENDING_TABLE_NAME, address);
+      let nextClaimTime = lastClaimTime + ONE_DAY;
+      if (nextClaimTime >= currentTime) {
+        res.send({
+          code: -2,
+          message:
+            'Already claimed, after ' +
+            new Date(nextClaimTime).toISOString() +
+            ' to claim.',
+        });
+        return;
+      }
+    }
+    if (StateDB.has(LOCAL_CLAIMED_TABLE_NAME, address)) {
+      lastClaimTime = StateDB.getValue(LOCAL_CLAIMED_TABLE_NAME, address);
+      let nextClaimTime = lastClaimTime + ONE_DAY;
+      if (nextClaimTime >= currentTime) {
+        res.send({
+          code: -2,
+          message:
+            'Already claimed, after ' +
+            new Date(nextClaimTime).toISOString() +
+            ' to claim.',
+        });
+        return;
+      }
+    }
+
+    console.log(
+      'Receive a faucet request from the account: ' +
+        address +
+        ' successfully!!!'
+    );
+    res.send({ code: 0, message: 'Receive a faucet request successfully' });
+    StateDB.setValue(LOCAL_PENDING_TABLE_NAME, address, currentTime);
     return;
   });
 };
